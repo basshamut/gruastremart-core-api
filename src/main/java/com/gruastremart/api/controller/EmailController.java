a14cfdc2811134d787cd760ba9c5fe128190253a
Jesus Fajardo <es.jfajardobass@gmail.com>
2025-03-10 23:51:34 +0100

    Adding mail endpoint

---
+++ src/main/java/com/gruastremart/api/controller/EmailController.java
@@ -0,0 +1,31 @@
+package com.gruastremart.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gruastremart.api.dto.EmailRequestDto;
+import com.gruastremart.api.service.EmailService;
+
+@RestController
+@RequestMapping("/v1/emails")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<?> sendContactEmail(@RequestBody EmailRequestDto emailRequest) {
+        try {
+            emailService.sendContactEmail(emailRequest);
+            return ResponseEntity.ok().body("Correo enviado correctamente");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error al enviar el correo: " + e.getMessage());
+        }
+    }
+}
